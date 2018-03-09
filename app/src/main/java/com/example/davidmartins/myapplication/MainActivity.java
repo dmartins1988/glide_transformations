cfdee9c4f3e136e0fb935e2ad917348e1e75aab0
dsbm88 <d.martins1988@gmail.com>
2018-03-09 17:20:37 +0000

    Add crop transformation

--- app/src/main/java/com/example/davidmartins/myapplication/MainActivity.java
+++ app/src/main/java/com/example/davidmartins/myapplication/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.davidmartins.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.imageCrop);
+
+
+        RequestOptions options = new RequestOptions();
+
+        final RequestOptions transform = options
+                .dontAnimate()
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .skipMemoryCache(true)
+                .transform(new CropTransformation(getBaseContext(), CropTransformation.CROP_BOTTOM));
+
+        Glide.with(getBaseContext())
+                .load(R.drawable.unnamed)
+                .apply(transform)
+                .into(mImageView);
+
     }
 }
---
+++ glideTransformations/src/main/java/com/example/glidetransformations/CropTransformation.java
@@ -0,0 +1,145 @@
+package com.example.glidetransformations;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.Transformation;
+import com.bumptech.glide.load.engine.Resource;
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapResource;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.security.MessageDigest;
+
+/**
+ * Created by david.martins on 09/03/2018.
+ */
+
+public class CropTransformation implements Transformation<Bitmap> {
+
+    @IntDef
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface CropSide {
+    }
+
+    public final int CROP_LEFT = 0;
+    public final int CROP_RIGHT = 1;
+    public final int CROP_TOP = 2;
+    public final int CROP_BOTTOM = 3;
+
+    private BitmapPool mBitmapPool;
+
+    private @CropSide
+    int mCropSide;
+
+    public CropTransformation(final Context context, @CropSide int cropSide) {
+        mCropSide = cropSide;
+        mBitmapPool = Glide.get(context).getBitmapPool();
+    }
+
+    @SuppressLint("SwitchIntDef")
+    @NonNull
+    @Override
+    public Resource<Bitmap> transform(@NonNull final Context context, @NonNull final Resource<Bitmap> resource, final int outWidth, final int outHeight) {
+
+        final Bitmap originalBitmap = resource.get();
+        Bitmap bitmapCropped = null;
+
+        switch (mCropSide) {
+            case CROP_LEFT:
+                bitmapCropped = cropLeftSide(originalBitmap);
+                break;
+            case CROP_RIGHT:
+                bitmapCropped = cropRightSide(originalBitmap);
+                break;
+            case CROP_TOP:
+                bitmapCropped = cropTopSide(originalBitmap);
+                break;
+            case CROP_BOTTOM:
+                bitmapCropped = cropBottomSide(originalBitmap);
+                break;
+        }
+
+        return BitmapResource.obtain(bitmapCropped, mBitmapPool);
+    }
+
+    @Override
+    public void updateDiskCacheKey(@NonNull final MessageDigest messageDigest) {
+
+    }
+
+    /**
+     * Crops bottom side of bitmap
+     *
+     * @param originalBitmap original bitmap
+     * @return cropped bitmap
+     */
+    private Bitmap cropBottomSide(final Bitmap originalBitmap) {
+        Bitmap cutBitmap = Bitmap.createBitmap(originalBitmap.getWidth(),
+                originalBitmap.getHeight() / 2, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(cutBitmap);
+        Rect desRect = new Rect(0, 0, originalBitmap.getWidth(), originalBitmap.getHeight() / 2);
+        Rect srcRect = new Rect(0, originalBitmap.getHeight() / 2, originalBitmap.getWidth(),
+                originalBitmap.getHeight());
+        canvas.drawBitmap(originalBitmap, srcRect, desRect, null);
+        return cutBitmap;
+    }
+
+    /**
+     * Crops top side of bitmap
+     *
+     * @param originalBitmap original bitmap
+     * @return cropped bitmap
+     */
+    private Bitmap cropTopSide(final Bitmap originalBitmap) {
+        Bitmap cutBitmap = Bitmap.createBitmap(originalBitmap.getWidth(),
+                originalBitmap.getHeight() / 2, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(cutBitmap);
+        Rect desRect = new Rect(0, originalBitmap.getHeight(), originalBitmap.getWidth(), originalBitmap.getHeight() / 2);
+        Rect srcRect = new Rect(0, originalBitmap.getHeight() / 2, originalBitmap.getWidth(),
+                originalBitmap.getHeight());
+        canvas.drawBitmap(originalBitmap, srcRect, desRect, null);
+        return cutBitmap;
+    }
+
+    /**
+     * Crops right side of bitmap
+     *
+     * @param originalBitmap original bitmap
+     * @return cropped bitmap
+     */
+    private Bitmap cropRightSide(final Bitmap originalBitmap) {
+        Bitmap cutBitmap = Bitmap.createBitmap(originalBitmap.getWidth() / 2,
+                originalBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(cutBitmap);
+        Rect desRect = new Rect(0, 0, originalBitmap.getWidth() / 2, originalBitmap.getHeight());
+        Rect srcRect = new Rect(originalBitmap.getWidth() / 2, 0, originalBitmap.getWidth(),
+                originalBitmap.getHeight());
+        canvas.drawBitmap(originalBitmap, srcRect, desRect, null);
+        return cutBitmap;
+    }
+
+    /**
+     * Crops lefts side of bitmap
+     *
+     * @param originalBitmap original bitmap
+     * @return cropped bitmap
+     */
+    private Bitmap cropLeftSide(final Bitmap originalBitmap) {
+        Bitmap cutBitmap = Bitmap.createBitmap(originalBitmap.getWidth() / 2,
+                originalBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(cutBitmap);
+        Rect desRect = new Rect(0, 0, originalBitmap.getWidth() / 2, originalBitmap.getHeight());
+        Rect srcRect = new Rect(0, 0, originalBitmap.getWidth(),
+                originalBitmap.getHeight());
+        canvas.drawBitmap(originalBitmap, srcRect, desRect, null);
+        return cutBitmap;
+    }
+}
